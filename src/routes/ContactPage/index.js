b3e0c3e9ae94a1bd7c8041ad6a84e0c4072fa2c9
Andrey <bars_skif@mail.ru>
2021-03-18 16:25:24 +0300

    'поправил верстку в aboutPage'

--- src/routes/ContactPage/index.js
+++ src/routes/ContactPage/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 
-const ContactPage = ()=> {
-return (
-<h1>ContactPage</h1>
-);
+const ContactPage = () => {
+    return (
+        <h1>ContactPage</h1>
+    );
 };
 
 export default ContactPage;
