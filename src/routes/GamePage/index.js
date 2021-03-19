fb17dd2b0fc23db6302f7eaa3ba7c6b5e6c3b24c
Andrey <bars_skif@mail.ru>
2021-03-19 18:01:40 +0300

    небольшие правки

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -65,7 +65,7 @@ const GamePage = () => {
         // });
 // console.log(arr[`${key}`])
         database.ref('pokemons/'+ key).update(
-            {active: !arr[`${key}`].active }
+            {active: !arr[key].active }
         )
 
     };
